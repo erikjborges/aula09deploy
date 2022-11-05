848343d51ead0c9639e8432d1ab6a3c036611953
esoft11 - Erik Borges <erikjborges@gmail.com>
2022-11-05 10:30:14 -0300

    Alterando mensagem de servidor rodando

--- src/infrastructure/apis/app/app.ts
+++ src/infrastructure/apis/app/app.ts
@@ -43,7 +43,7 @@ routes.push(new AccountsRoutes(app));
 routes.push(new AuthRoutes(app));
 // routes.push(new TransactionsRoutes(app));
 
-const runningMessage = `Servidor rodando na porta ${port}`;
+const runningMessage = `=D Servidor rodando na porta ${port}`;
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(runningMessage);
 })
